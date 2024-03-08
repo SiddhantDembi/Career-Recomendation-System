8483fb5776a7fffb4661e33f12b13dca4084900c
Siddhant Dembi <siddhantdembi@gmail.com>
2024-03-09 01:30:43 +0530

    Delete unused images, styles, components, and Redux features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import store from './redux/store'
-import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login";
 import Sidebar from './pages/sidebar'
-import Register from "./pages/Register";
-import { useSelector } from "react-redux";
-import Spinner from "./components/Spinner";
-import ProtectedRoute from "./components/ProtectedRoute";
-import PublicRoute from "./components/PublicRoute";
-
-import Header from "./pages/Header";
 import DashboardTemplate from "./pages/DashboardTemplate";
-import Application from "./pages/Application";
-
-import Apply from "./pages/Apply";
 import Difficulties from "./pages/Difficulties";
-import Address from "./pages/Address";
 import Resources from "./pages/ResourcePage";
-import About from './pages/About'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import Footer from './pages/Footer'
 import Chatbot from './pages/Chatbot'
 import JobList from './pages/JobList'
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Provider store={store}>
 
       <BrowserRouter>
         { (
@@ -52,33 +29,8 @@ function App() {
                 <><Chatbot/><Sidebar/></>
               }
               />
-            <Route
-              path="/login"
-              element={
-                <PublicRoute>
-                {/* <ProtectedRoute> */}
-                  <Login />
-                  {/* </ProtectedRoute> */}
-                </PublicRoute>
-              }
-              />
-            <Route
-              path="/register"
-              element={
-                <PublicRoute>
-                {/* <ProtectedRoute> */}
-                  <Register />
-                {/* </ProtectedRoute> */}
-                </PublicRoute>
-              }
-              />
-            <Route
-              path="/application"
-              element={
-                <><Application /><Sidebar/></>
-                
-              }
-              />
+            
+            
             <Route
               path="/resources"
               element={
@@ -92,30 +44,7 @@ function App() {
                 <><Difficulties /><Sidebar/></>
               }
               />
-            <Route
-              path="/address"
-              element={
-                <><Address /><Sidebar/></>
-              }
-              />
-            <Route
-              path="/about"
-              element={
-                <><About/><Footer/></>
-              }
-              />
-            <Route
-              path="/home1"
-              element={
-                <><Home/><About/><Contact/><Footer/></>
-              }
-              />
-            <Route
-              path="/contact"
-              element={
-                <><Contact/><Footer/></>
-              }
-              />
+            
             <Route
               path="/joblist"
               element={
@@ -125,7 +54,6 @@ function App() {
           </Routes>
         )}
       </BrowserRouter>
-      </Provider>
     </>
   )
 }
--- frontend/src/pages/Sidebar.jsx
+++ frontend/src/pages/Sidebar.jsx
@@ -1,56 +1,11 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-// import { useState } from "react";
-// import { Line } from "react-chartjs-2";
-// import {
-//   Chart,
-//   CategoryScale,
-//   LinearScale,
-//   LineController,
-//   PointElement,
-//   LineElement,
-//   Title,
-// } from "chart.js";
-import Header from "../pages/Header";
 import academic_managementIcon from "../assets/academic_managementIcon.svg";
-// import resource_managementIcon from "../assets/resource_managementIcon.svg";
-// import lifestyleIcon from "../assets/lifestyleIcon.svg";
 import dashboardIcon from "../assets/dashboardIcon.svg";
-// import skillIcon from "../assets/skillIcon.svg";
-// import campusIcon from "../assets/campusIcon.svg";
-import subMenuIcon from "../assets/subMenuIcon.svg";
-// import helpIcon from "../assets/helpIcon.svg";
-import "../DashboardTemplate.css";
-import Application from "./Application";
-// import { Route } from "react-router-dom";
-// import Apply from "./jobs";
-import Address from "./Address";
-// import emblem from "../components/images/img1.jpg"
-// import photo from "../components/images/img2.jpg"
-import dashboardImg1 from "../images/img1.jpg"
-import dashboardImg2 from "../images/img2.png"
+import "../styles/DashboardTemplate.css";
 
 
 function Sidebar() {
 const [isSBVisible, setIsSBVisible] = useState([]);
-
-const sidebarClick = (key) => {
-    // setIsSBVisible((prevISVisible) => {
-    //   let visibleAarry = [...prevISVisible];
-    //   visibleAarry[key] = !prevISVisible[key];
-    //   return visibleAarry;
-    // });
-    
-    sidebarData.map((data) => {
-    if (key === data.id) {
-        return (
-        <>
-            <Application />
-        </>
-        );
-    }
-    });
-};
 
 const sidebarData = [
     {
@@ -88,7 +43,7 @@ return (
     <aside>
     {sidebarData.map((data) => (
         <div className="sidebar-elements" key={data.id}>
-        <a href={data.href} className="btn-click">
+        <a style={{textDecoration: "none"}} href={data.href} className="btn-click">
             <img src={data.image} alt={data.text} />
             {data.text}
         </a>
